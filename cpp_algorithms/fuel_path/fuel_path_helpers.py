c86bed6b48a990dc7935e8694b5bd3855ac86a74
18alantom <2.alan.tom@gmail.com>
2020-07-27 05:09:36 +0530

    Fix fuelling

--- cpp_algorithms/fuel_path/fuel_path_helpers.py
+++ cpp_algorithms/fuel_path/fuel_path_helpers.py
@@ -30,6 +30,7 @@ def path_to_fuel(dist_map, loc, fuel=FU, obs=OB):
     fuel : value of fuel on the `dist_map`
     obs : value of obstacle on the `dist_map`
     """
+    loc = tuple(loc)
     path = [loc]
     cell = dist_map[loc]
     while cell != fuel:
@@ -37,66 +38,56 @@ def path_to_fuel(dist_map, loc, fuel=FU, obs=OB):
         path.append(loc)
     return path
 
-def get_refuel_idx(dist_map, path, fuel_cap):
+def get_refuel_idx(dist_map, coverage_path, fuel_cap):
     """
     PARAMETERS
     ---
     dist_map : distance map where fuel points are 0 valued, 
         (m,n) shape nd array
-    path : sequence of coords that traverses the `dist_map`.
+    coverage_path : sequence of coords that traverses the `dist_map`.
         list of tuples, [(x1,y1),...,(xm,ym)]
     fuel_cap : capacity of the fuel required by the drone, int.
     
     RETURNS
     ---
-    points : indices in the path where refuelling detour is taken.
-    fcap : fuel capacity at every point in the path.
+    points : indices in the coverage_path where refuelling detour is taken.
+    fcap : fuel capacity at every point in the coverage_path.
     """
-    path = np.array(path)
-    x,y = path.T
+    coverage_path = np.array(coverage_path)
+    x,y = coverage_path.T
     dist = dist_map.copy()[x,y]
     plen = dist.shape[0]
     fcap = np.full(dist.shape, 0)
 
     points = []
 
-    cu = 0 # path distance travelled by the drone
+    cu = 0 # coverage_path distance travelled by the drone
     fuel_req = 0 # initial req 0 cause starting with full tank
 
+    i = 0
     while True:
+        i+=1
         ini_fuel = (fuel_cap - fuel_req)
-
-        assert ini_fuel > 0, "no fuel left to complete path" 
-
-        """
-        Start index : how many points have previously been covered
-        End index   : how many points can be covered with available fuel
-        """
+        assert ini_fuel > 0, "no fuel left to complete coverage_path" 
+        # start : how many points have previously been covered.
+        # end   : how many points can be covered with available fuel.
         start = cu
         end = cu + ini_fuel
         
-        """
-        If end index overshoots path length,
-        end index should be the path length (ie last position -1)
-        """
+        # If end index overshoots coverage_path length,
+        # end index should be the coverage_path length (ie last position -1)
         if plen - end < 0:
             end = plen
         sl = slice(start, end)
 
-        """
-        Amount of distance that can be travelled by the drone
-        taking into account the last loc.
-        """
+        # Amount of distance that can be travelled by the drone
+        # taking into account the last loc.
         p_seg_len = end - start
         fin_fuel = ini_fuel - p_seg_len
 
-        """
-        Possible fuel remaining starting index 
-        """
+        # Possible fuel remaining starting index 
         fcap[sl] = np.arange(ini_fuel, fin_fuel, -1)
-        """
-        if : Remaining fuel is sufficient.
-        """
+        # if : Remaining fuel is sufficient.
         if ini_fuel - (plen - cu) >=0:
             break
 
@@ -104,7 +95,10 @@ def get_refuel_idx(dist_map, path, fuel_cap):
         try:
             idx = freq[freq >= 0].argmin()
         except ValueError:
-            raise ValueError("path can't be completed, insufficient fuel capacity")
+            raise ValueError("coverage path can't be traversed, insufficient fuel capacity")
+        
+        if len(points) > 1 and points[-1] == points[-2]:
+            raise ValueError(f"coverage path can't be traversed, insufficient fuel capacity\n stuck at coverage path index :{points[-1]}")
 
         cu += idx
 
