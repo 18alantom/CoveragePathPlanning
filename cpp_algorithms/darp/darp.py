831aa6e37c52577627a0199a1100b0354e177319
18alantom <2.alan.tom@gmail.com>
2020-08-03 13:26:04 +0530

    add None

--- cpp_algorithms/darp/darp.py
+++ cpp_algorithms/darp/darp.py
@@ -5,6 +5,7 @@ from .continuity import get_flood_matrix
 from .darp_helpers import get_evaluation_matrices, get_c
 from .darp_helpers import get_no_obs_count, get_coverage_ratio
 
+
 def darp(epochs, area_map, start_points, drone_speed=None, drone_coverage=None, use_flood=True, pbar=False, obstacle=OB):
     """
     Runs DARP on the given area map and returns the assignement matrix.
@@ -26,16 +27,17 @@ def darp(epochs, area_map, start_points, drone_speed=None, drone_coverage=None, 
     n = len(start_points)
     c = get_c(area_map)
     E = get_evaluation_matrices(start_points, area_map)
-    
+
+    use_flood = None
     # Flood matrix
     if use_flood:
-        flood_matrix = get_flood_matrix(area_map,pbar=pbar)
-        
+        flood_matrix = get_flood_matrix(area_map, pbar=pbar)
+
     if drone_coverage is None or drone_speed is None:
-        drone_speed = np.ones(n)        
-        drone_coverage = np.ones(n)     
-        
+        drone_speed = np.ones(n)
+        drone_coverage = np.ones(n)
+
     nobs_count = get_no_obs_count(area_map)
     coverage_ratio = get_coverage_ratio(drone_speed, drone_coverage)
-    
+
     return iterate(epochs, start_points, E, c, nobs_count, coverage_ratio, flood_matrix, pbar=pbar)
